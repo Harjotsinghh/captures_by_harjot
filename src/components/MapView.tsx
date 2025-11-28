de30bf82f3867ea44117b1d170b5b59913fbce76
Harjot Singh <harjotsingh206@gmail.com>
2025-11-28 22:40:28 +0530

    update padding inside map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,9 +1,4 @@
-import {
-  MapContainer,
-  TileLayer,
-  Polyline,
-  useMap,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Polyline, useMap } from "react-leaflet";
 import { useEffect, useMemo, memo } from "react";
 import L from "leaflet";
 import type { Photo } from "../data/images";
@@ -32,15 +27,15 @@ const AutoCenterMap = memo<{ places: any[] }>(({ places }) => {
 
   useEffect(() => {
     if (places.length > 0) {
-      const bounds = L.latLngBounds(places.map(p => [p.lat, p.lng]));
-      map.fitBounds(bounds, { padding: [80, 80] });
+      const bounds = L.latLngBounds(places.map((p) => [p.lat, p.lng]));
+      map.fitBounds(bounds, { padding: [200, 200] });
     }
   }, [places, map]);
 
   return null;
 });
 
-AutoCenterMap.displayName = 'AutoCenterMap';
+AutoCenterMap.displayName = "AutoCenterMap";
 
 function MapView({ images, onMarkerClick }: MapViewProps) {
   const { center, places } = useMapData(images);
@@ -52,14 +47,24 @@ function MapView({ images, onMarkerClick }: MapViewProps) {
   );
 
   const tileUrl = useMemo(
-    () => theme === 'dark'
-      ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-      : "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+    () =>
+      theme === "dark"
+        ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        : "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
     [theme]
   );
 
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, overflow: 'hidden' }}>
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflow: "hidden",
+      }}
+    >
       <MapContainer
         center={center}
         zoom={6}
@@ -79,7 +84,7 @@ function MapView({ images, onMarkerClick }: MapViewProps) {
         <Polyline
           positions={coords}
           pathOptions={{
-            color: theme === 'dark' ? "#a3a3a3" : "#555",
+            color: theme === "dark" ? "#a3a3a3" : "#555",
             weight: 2,
             opacity: 0.6,
             dashArray: "10, 10",
